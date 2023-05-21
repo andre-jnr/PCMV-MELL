564df97b98aae568c3d3b10356d11a10616956c3
André Júnior <andrejunior.pessoal@gmail.com>
2023-05-20 20:03:05 -0400

    intregação entre páginas

--- login.js
+++ login.js
@@ -1,31 +1,36 @@
-const passwordInput = document.getElementById("senha","senha2")
-const olho = document.getElementById("olho")
-const olho2 = document.getElementById("olho2")
+const passwordInput = document.getElementById('senha', 'senha2')
+const olho = document.getElementById('olho')
+const olho2 = document.getElementById('olho2')
 
-function abrir(){
-    let typePassword = passwordInput.type =="password"
+function abrir() {
+  let typePassword = passwordInput.type == 'password'
 
-
-if (typePassword) {
+  if (typePassword) {
     showPassword()
-
-} else{
+  } else {
     hidePassword()
-
-}
-
+  }
 }
 
 function showPassword() {
-    passwordInput.setAttribute("type", "text")
-    olho.setAttribute("src", "imagem/open.png")
-    olho2.setAttribute("src", "imagem/open.png")
-    
+  passwordInput.setAttribute('type', 'text')
+  olho.setAttribute('src', 'imagem/open.png')
+  olho2.setAttribute('src', 'imagem/open.png')
 }
 
 function hidePassword() {
-    passwordInput.setAttribute("type", "password")
-    olho.setAttribute("src", "imagem/closed.png")
-    olho2.setAttribute("src", "imagem/closed.png")
-   
+  passwordInput.setAttribute('type', 'password')
+  olho.setAttribute('src', 'imagem/closed.png')
+  olho2.setAttribute('src', 'imagem/closed.png')
+}
+
+function logar() {
+  var usuario = document.getElementById('usuario').value
+  var senha = document.getElementById('senha').value
+
+  if (usuario == 'jeane' && senha == '12345') {
+    location.href = 'index.html'
+  } else {
+    alert('Usuário ou senha inválidos!!!')
+  }
 }
